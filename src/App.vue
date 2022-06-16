a9f64ff88c3957ae854bcfb4d967e770259a141b
Hengtai Wang <hengtai.wang@cummins.com>
2022-06-16 17:38:48 +0800

    创建复现仓库

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+const show = ref(false);
+</script>
+<script>
+import { Dialog } from "vant";
+
+export default {
+  components: {
+    [Dialog.Component.name]: Dialog.Component,
+  },
+};
+</script>
+
+<template>
+<button @click="show=!show">toggle</button>
+  <van-dialog v-model:show="show" title="标题" show-cancel-button>
+    <img src="https://fastly.jsdelivr.net/npm/@vant/assets/apple-3.jpeg" />
+  </van-dialog>
+</template>
