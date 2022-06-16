dd5e5306a62dfb696b4887f341592c952f78dec8
Hengtai Wang <hengtai.wang@cummins.com>
2022-06-16 17:41:10 +0800

    创建复现仓库

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,10 @@ export default {
 </script>
 
 <template>
-<button @click="show=!show">toggle</button>
+  <button @click="show = !show">toggleShow</button>
+  <div>
+    show:<span>{{ show }}</span>
+  </div>
   <van-dialog v-model:show="show" title="标题" show-cancel-button>
     <img src="https://fastly.jsdelivr.net/npm/@vant/assets/apple-3.jpeg" />
   </van-dialog>
